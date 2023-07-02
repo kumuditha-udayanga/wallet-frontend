f61b6f9c69b642f388074059e1761b3ea2616900
kumuditha-udayanga <kumudithaudaya@gmail.com>
2023-07-02 14:16:20 +0530

    Create basic view

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,37 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
-import {Card, List, Spin} from "antd";
+import {Card, List, notification, Spin} from "antd";
 import { Expense } from "./types";
-import { getExpenses } from "./services/expense-services";
+import axios, {AxiosResponse} from "axios";
+import {API_URL} from "./constants";
 
 function App() {
     const [expenses, setExpenses] = useState<Expense[]>([]);
     const [isLoading, setIsLoading] = useState(false);
 
+    const getExpenses = async () => {
+        axios.get(
+                `${API_URL}expense/`
+            ).then((result: AxiosResponse<Expense[]>) => {
+                if (result.status === 200 || result.status === 204) {
+                    setExpenses(result.data)
+                    setIsLoading(false);
+                } else {
+                    setIsLoading(false);
+                    throw new Error();
+                }
+            }).catch(() => {
+                setIsLoading(false);
+                notification.error({
+                    message: 'Warning!',
+                    description: 'Something went wrong when fetching expenses'
+                });
+        })
+    }
+
     useEffect(() => {
-        setIsLoading(true);
-        // setExpenses();
+        setIsLoading(true) ;
+        getExpenses();
     }, []);
 
     return (
@@ -21,7 +42,7 @@ function App() {
             dataSource={expenses}
             renderItem={(item) => (
                 <List.Item>
-                    <Card title={item.title}>Card content</Card>
+                    <Card title={item.title}>{item.description}</Card>
                 </List.Item>
             )}
         />
